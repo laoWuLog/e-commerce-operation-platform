5fe02c8d030d74169199412a1b61117511d34d99
独此一家 <wu669937@163.com>
2022-03-15 21:10:59 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-row class="mb-4">
+    <el-button>Default</el-button>
+  </el-row>
+</template>
+<script lang="ts" setup>
+import { reactive, toRefs, ref } from "vue";
+</script>
+<style lang="less">
+@import url("./assets/styles/index.less");
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
